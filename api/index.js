a16ac5bd665fb625fe44b3debca3c1ff58f35d79
Jeel <viradiyajeel@gmail.com>
2024-05-21 14:52:28 +0530

    create middleware and errorHandling function

--- api/index.js
+++ api/index.js
@@ -1,25 +1,36 @@
-import express from "express";
-import mongoose from "mongoose";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import dotenv from "dotenv";
-dotenv.config();
+import express from "express"
+import mongoose from "mongoose"
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
+import dotenv from "dotenv"
+dotenv.config()
 
-const app = express();
+const app = express()
 app.use(express.json())
 
 mongoose
   .connect(process.env.DATABASE_URI)
   .then(() => {
-    console.log("Connection established successfully!!");
+    console.log("Connection established successfully!!")
   })
   .catch((err) => {
-    console.error("Error in connection : " + err);
+    console.error("Error in connection : " + err)
   });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  console.log("Server is running on port 3000")
+})
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
