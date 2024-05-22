7ee4e1a726b01a25d278db30ad81d0bcda8244fe
Jeel <viradiyajeel@gmail.com>
2024-05-22 15:44:02 +0530

    complete sign-up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -11,7 +11,7 @@ export const signup = async (req,res, next) => {
         await newUser.save()
         res.status(201).json({message: "User created Successfully!"})
     } catch (error) {
-        next(errorHandler(550, "error from signup function"))
+        next(errorHandler(error.statusCode , error.message))
     }
 
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.pages.jsx'
 import SignIn from './pages/SignIn.pages.jsx'
 import SignUp from './pages/SignUp.pages.jsx'
