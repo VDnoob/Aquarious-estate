b4cf337cfdf14bece1ae0485eefb80ac79f07eec
Jeel <viradiyajeel@gmail.com>
2024-06-16 14:25:35 +0530

    Create Listing api route

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
-import path from 'path';
 dotenv.config();
 
 mongoose
@@ -15,8 +15,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-  const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,13 +28,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.use('/api/listing',listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
